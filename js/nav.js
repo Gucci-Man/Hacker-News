e60b27938f789df71bd37cebecf7bc7beedebb6e
Adel <adel.ngo@yahoo.com>
2023-07-29 01:26:36 -0500

    Updated all code

--- js/nav.js
+++ js/nav.js
@@ -18,12 +18,34 @@ $body.on("click", "#nav-all", navAllStories);
 /** When user clicks the submit link, it takes them to a form to add a story */
 
 function navAddStory(evt) {
-  console.debug("navAddStory");
+  console.debug("navAddStory", evt);
   hidePageComponents();
   $addStoryForm.show();
+  $allStoriesList.show();
 }
 
 $navAddStory.on("click", navAddStory);
+
+/** Show favorite stories on click on "favorites" */
+
+function navFavoritesClick(evt) {
+  console.debug("navFavoritesClick", evt);
+  hidePageComponents();
+  putFavoritesListOnPage();
+}
+
+$body.on("click", "#nav-favorites", navFavoritesClick);
+
+/** Show My Stories on clicking "my stories" */
+
+function navMyStories(evt) {
+  console.debug("navMyStories", evt);
+  hidePageComponents();
+  putUserStoriesOnPage();
+  $ownStories.show();
+}
+
+$body.on("click", "#nav-my-stories", navMyStories);
 
 /** Show login/signup on click on "login" */
 
@@ -50,5 +72,15 @@ function updateNavOnLogin() {
   $navAddStory.show(); // show option to add story 
   $navUserProfile.text(`${currentUser.username}`).show();
 }
+
+/** Hide everything but profile on click on "profile" */
+
+function navProfileClick(evt) {
+  console.debug("navProfileClick", evt);
+  hidePageComponents();
+  $userProfile.show();
+}
+
+$navUserProfile.on("click", navProfileClick);
 
 
